ee31931e969ab89fca9984fc8d92645edd08cb21
Afooseyi <afooseyi@gmail.com>
2023-04-15 05:33:41 -0700

    first commit

--- status_server/api/v1/app.py
+++ status_server/api/v1/app.py
@@ -8,6 +8,9 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.route('/status', methods=['GET'])
+def api_status():
+    return jsonify({'status': 'ok'})
 
 @app.errorhandler(404)
 def not_found(error):
